efa53ad38ac0332eda53d20868f32a18c94b95da
IsabelSleigman <sleimusic@gmail.com>
2022-01-22 19:09:06 -0300

    feito o desafio hohoho

---
+++ HO HO HO/HO HO HO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HO_HO_HO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Diga a quantidade desejada");
+            int N = int.Parse(Console.ReadLine());
+
+            //Exibir "Ho" do papai noel
+            for (int i = 0; i < N; i++)
+            {
+                if (i < (N - 1))
+                {
+                    Console.Write("Ho ");
+                }
+                else
+                {
+                    Console.WriteLine("Ho!");
+                }
+            }
+        }
+    }
+}
