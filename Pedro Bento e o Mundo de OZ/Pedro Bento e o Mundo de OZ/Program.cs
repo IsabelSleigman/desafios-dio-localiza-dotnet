9a7e3a758e3cbbd6feaf998e988a1e99208b2c8e
IsabelSleigman <sleimusic@gmail.com>
2022-01-22 19:23:41 -0300

    Desafio Pedro Bento e o Mundo de OZ

---
+++ Pedro Bento e o Mundo de OZ/Pedro Bento e o Mundo de OZ/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pedro_Bento_e_o_Mundo_de_OZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> joias = new List<string>();
+            bool aindaTemJoias;
+
+            do
+            {
+                Console.WriteLine("Informe o modelo da joia");
+                var joia = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(joia))
+                {
+                    joias.Add(joia);
+                    aindaTemJoias = true;
+                }
+                else
+                {
+                    aindaTemJoias = false;
+                }
+            } while (aindaTemJoias);
+
+            Console.WriteLine(joias.Distinct().Count());
+        }
+    }
+}
