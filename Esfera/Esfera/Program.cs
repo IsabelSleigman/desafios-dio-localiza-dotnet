46d0e2e204d2e72a91550f6118be228d740b4411
IsabelSleigman <sleimusic@gmail.com>
2022-01-22 19:17:14 -0300

    desafio esfera

---
+++ Esfera/Esfera/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Esfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi, raio, volume;
+            pi = 3.14159;
+
+            Console.WriteLine("Diga o valor ");
+            raio = double.Parse(Console.ReadLine());
+
+            volume = (4.0 / 3) * pi * Math.Pow(raio, 3);
+
+            Console.WriteLine("VOLUME = {0:0.000}", volume);
+        }
+    }
+}
