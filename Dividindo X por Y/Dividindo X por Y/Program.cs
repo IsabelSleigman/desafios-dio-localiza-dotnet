648abd89b12697696c2ed59ade22fdac9f8f340f
IsabelSleigman <sleimusic@gmail.com>
2022-01-22 19:13:08 -0300

    desafio divisão

---
+++ Dividindo X por Y/Dividindo X por Y/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dividindo_X_por_Y
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Diga os valores");
+            int limit = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < limit; i++)
+            {
+                string[] line = Console.ReadLine().Split(" ");
+                double X = double.Parse(line[0]);
+                double Y = double.Parse(line[1]);
+
+                Console.WriteLine((Y == 0) ? "divisao impossivel" : $"{(X / Y).ToString("N1")}");
+
+            }
+        }
+    }
+}
