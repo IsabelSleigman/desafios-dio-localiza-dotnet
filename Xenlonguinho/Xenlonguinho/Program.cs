d8c2afbe9c057cd4f97f19466e1ab5af150927d7
IsabelSleigman <sleimusic@gmail.com>
2022-01-22 19:20:10 -0300

    desafio xenlonguinho

---
+++ Xenlonguinho/Xenlonguinho/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Xenlonguinho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Diga o valor");
+            int numero = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numero; i++)
+            {
+                int qtdEsferas = int.Parse(Console.ReadLine());
+                int contEsferas = 0;
+
+                for (int j = 1; j <= qtdEsferas; j++)
+                {
+                    int contDivisores = 0;
+
+                    for (int qtdEstrelas = 1; qtdEstrelas <= j; qtdEstrelas++)
+                    {
+                        if (j % qtdEstrelas == 0) contDivisores++;
+                    }
+
+                    if (contDivisores % 2 == 0)
+                    {
+                        contEsferas++;
+                    }
+
+                }
+
+                Console.WriteLine(contEsferas);
+            }
+        }
+    }
+}
